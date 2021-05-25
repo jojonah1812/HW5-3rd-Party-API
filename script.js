1d48540819923bd8a46d8da74c69120c832ddd98
Jank Jank <jank.jank1812@gmail.com>
2021-05-25 14:48:22 -0700

    pre-save button interactibility

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 var timeDisplayEl = $('#time-display');
 
-
-
 function displayTime() {
     var rightNow = moment().format('dddd, MMMM Do YYYY');
     //update ID time display with info from rightNow
     timeDisplayEl.text(rightNow);
     }
-
 setInterval(displayTime, 1000);
+
+// timeblocks
