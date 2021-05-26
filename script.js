8ed6426f0b993336a0bbccb393b52b61f1f6b6bd
Jank Jank <jank.jank1812@gmail.com>
2021-05-25 19:46:46 -0700

    progress on Local STorage, unfisnished, but hour8am done

--- script.js
+++ script.js
@@ -9,12 +9,30 @@ setInterval(displayTime, 1000);
 
 // timeblocks
 
+var text = "textArea"
 
 document.getElementById("saveBtn").addEventListener("click", saveToLocal);
 
 function saveToLocal() {
+    localStorage.setItem("textArea", text);
+    }
 
-    localStorage.setItem("key", "value");
 
-    //   document.getElementById("demo").innerHTML = Date();
-}
+
+//GET ITEM FROM LOCAL STORAGE
+$("#hour8am.textArea").val(localStorage.getItem("hour8am"))
+
+
+
+
+    // Get item from local storage if any
+    $("#hour8 .description").val(localStorage.getItem("hour8"));
+    // $("#hour9 .description").val(localStorage.getItem("hour9"));
+    // $("#hour10 .description").val(localStorage.getItem("hour10"));
+    // $("#hour11 .description").val(localStorage.getItem("hour11"));
+    // $("#hour12 .description").val(localStorage.getItem("hour12"));
+    // $("#hour13 .description").val(localStorage.getItem("hour13"));
+    // $("#hour14 .description").val(localStorage.getItem("hour14"));
+    // $("#hour15 .description").val(localStorage.getItem("hour15"));
+    // $("#hour16 .description").val(localStorage.getItem("hour16"));
+    // $("#hour17 .description").val(localStorage.getItem("hour17"));
