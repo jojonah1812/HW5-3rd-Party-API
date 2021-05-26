c395b5209e7b1fc8582b4e436f233fa70642bd73
Jank Jank <jank.jank1812@gmail.com>
2021-05-26 16:32:57 -0700

    just cleared angry red marks from js file - I was missing a }); at the end of my jquery Local Storage

--- script.js
+++ script.js
@@ -4,13 +4,14 @@
 // 3. TRASH not working
 // 4. colors not working by time
 
-
-
 // Timer //
+var rightNow = moment().format('dddd, MMMM Do YYYY hh:mm:ss a');
+
+
 var timeDisplayEl = $('#time-display');
 
 function displayTime() {
-    var rightNow = moment().format('dddd, MMMM Do YYYY hh:mm:ss a');
+    
     //update ID time display with info from rightNow
     timeDisplayEl.text(rightNow);
     console.log(rightNow)
@@ -51,12 +52,7 @@ $(document).ready(function() {
         // save in localStorage
         localStorage.setItem(time, value);
     });
-
-
-
-
-
-
+});
 
 
 
@@ -65,6 +61,10 @@ $(document).ready(function() {
 
 
 // TIME COLORS //
+
+
+
+
 
 function timeColors() {
     //current number hours//
@@ -92,15 +92,15 @@ function timeColors() {
 }
 
 //GET ITEM FROM LOCAL STORAGE
-$("#hour8.textArea").val(localStorage.getItem("hour8"))
-$("#hour9.textArea").val(localStorage.getItem("hour9"))
-$("#hour10.textArea").val(localStorage.getItem("hour10"))
-$("#hour11.textArea").val(localStorage.getItem("hour11"))
-$("#hour12.textArea").val(localStorage.getItem("hour12"))
-$("#hour13.textArea").val(localStorage.getItem("hour13"))
-$("#hour14.textArea").val(localStorage.getItem("hour14"))
-$("#hour15.textArea").val(localStorage.getItem("hour15"))
-$("#hour16.textArea").val(localStorage.getItem("hour16"))
-$("#hour17.textArea").val(localStorage.getItem("hour17"))
+$("#hour8.textArea").val(localStorage.getItem("hour8"));
+$("#hour9.textArea").val(localStorage.getItem("hour9"));
+$("#hour10.textArea").val(localStorage.getItem("hour10"));
+$("#hour11.textArea").val(localStorage.getItem("hour11"));
+$("#hour12.textArea").val(localStorage.getItem("hour12"));
+$("#hour13.textArea").val(localStorage.getItem("hour13"));
+$("#hour14.textArea").val(localStorage.getItem("hour14"));
+$("#hour15.textArea").val(localStorage.getItem("hour15"));
+$("#hour16.textArea").val(localStorage.getItem("hour16"));
+$("#hour17.textArea").val(localStorage.getItem("hour17"));
 
 timeColors();
