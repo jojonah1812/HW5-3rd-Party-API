9147628d1c779aad3d306a58903c95929647426f
Jank Jank <jank.jank1812@gmail.com>
2021-05-26 22:08:30 -0700

    made many attempt to get the bugs worked out, but I was unable to add anything with the local storage, colors per past/present/future without breaking the timer.

--- script.js
+++ script.js
@@ -10,41 +10,7 @@ function displayTime() {
     timeDisplayEl.text(rightNow);
 }
 
-////////////////////////////////////////
-// // Timer //
-// var rightNow = moment().format('dddd, MMMM Do YYYY hh:mm:ss a');
-// var timeDisplayEl = $('#time-display');
-// function displayTime() {
-//     //update ID time display with info from rightNow
-//     timeDisplayEl.text(rightNow);
-//     console.log(rightNow)
-//     // setInterval(displayTime, 1000);
-// }
-//////////////////////////////////////////////////////
-
-
-//// Local Storage ////
-///multiple save buttons///
-// var saveButtonVar = document.querySelectorAll(".saveBtn")
-
-// //27-30 applying eventlistener to each button with a class saveBtn
-// for (let i = 0; i < saveButtonVar.length; i++) {
-//     saveButtonVar[i].addEventListener("click", saveToLocal);
-
-// }
-// //this = saveButton
-// function saveToLocal() {
-//     var time = this
-//     var text    
-//  textAreaVar = document.querySelectorAll(".textArea").value
-//     localStorage.setItem(".textArea", text);
-//     }
-
-
-
-
-
-
+// // Local Storage ////
 
 $(document).ready(function() {
     $(".saveBtn").on("click", function() {
@@ -57,38 +23,6 @@ $(document).ready(function() {
     });
 });
 
-
-
-
-
-
-
-// // TIME COLORS //
-// function timeColors() {
-//     //current number hours//
-//     var rightNow = moment().hour();
-
-//     $(".time-block").each(function() {
-//         var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-    
-//         if (blockTime < rightNow) {
-//             $(this).addClass("past");
-//             $(this).removeClass("present");
-//             $(this).removeClass("future");
-//         }
-//         else if (blockTime=== rightNow) {
-//             $(this).removeClass("past");
-//             $(this).addClass("present");
-//             $(this).removeClass("future");
-//         }
-//         else {
-//             $(this).removeClass("past");
-//             $(this).removeClass("present");
-//             $(this).addClass("future");
-//         }
-//     })    
-// }
-
 // //GET ITEM FROM LOCAL STORAGE
 // $("#hour8.textArea").val(localStorage.getItem("hour8"));
 // $("#hour9.textArea").val(localStorage.getItem("hour9"));
@@ -100,6 +34,33 @@ $(document).ready(function() {
 // $("#hour15.textArea").val(localStorage.getItem("hour15"));
 // $("#hour16.textArea").val(localStorage.getItem("hour16"));
 // $("#hour17.textArea").val(localStorage.getItem("hour17"));
+
+
+// TIME COLORS //
+function timeColors() {
+    //current number hours//
+    var rightNow = moment().hour();
+
+    $(".time-block").each(function() {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+    
+        if (blockTime < rightNow) {
+            $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+        }
+        else if (blockTime=== rightNow) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        }
+    })    
+}
 
 // timeColors();
 setInterval(displayTime, 1000);
