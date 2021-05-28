1f42513d7c557ccbb822e918249fa5631eebeec6
Jank Jank <jank.jank1812@gmail.com>
2021-05-28 14:54:55 -0700

    Local storage now working with Set and Get.  As a result the timer has stopped functioning.

--- script.js
+++ script.js
@@ -23,17 +23,19 @@ $(document).ready(function() {
     });
 });
 
+
+
 // //GET ITEM FROM LOCAL STORAGE
-// $("#hour8.textArea").val(localStorage.getItem("hour8"));
-// $("#hour9.textArea").val(localStorage.getItem("hour9"));
-// $("#hour10.textArea").val(localStorage.getItem("hour10"));
-// $("#hour11.textArea").val(localStorage.getItem("hour11"));
-// $("#hour12.textArea").val(localStorage.getItem("hour12"));
-// $("#hour13.textArea").val(localStorage.getItem("hour13"));
-// $("#hour14.textArea").val(localStorage.getItem("hour14"));
-// $("#hour15.textArea").val(localStorage.getItem("hour15"));
-// $("#hour16.textArea").val(localStorage.getItem("hour16"));
-// $("#hour17.textArea").val(localStorage.getItem("hour17"));
+$("#hour8.textArea").val(localStorage.getItem("hour8"));
+$("#hour9.textArea").val(localStorage.getItem("hour9"));
+$("#hour10.textArea").val(localStorage.getItem("hour10"));
+$("#hour11.textArea").val(localStorage.getItem("hour11"));
+$("#hour12.textArea").val(localStorage.getItem("hour12"));
+$("#hour13.textArea").val(localStorage.getItem("hour13"));
+$("#hour14.textArea").val(localStorage.getItem("hour14"));
+$("#hour15.textArea").val(localStorage.getItem("hour15"));
+$("#hour16.textArea").val(localStorage.getItem("hour16"));
+$("#hour17.textArea").val(localStorage.getItem("hour17"));
 
 
 // TIME COLORS //
@@ -63,4 +65,12 @@ function timeColors() {
 }
 
 // timeColors();
-setInterval(displayTime, 1000);
+// setInterval(displayTime, 1000);
+
+
+
+
+
+
+
+
